658c4368ade2732d8c6a4aecebcc68c0126b44e8
LEVHARZ <edwardhernan79@gmail.com>
2024-05-26 18:36:14 -0600

    finalización del diseño y funcionalidad de la versión desktop

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const sideBar = document.querySelector('.slide--menu');
 const cuerpo = document.querySelector('body');
 const nav = document.querySelector('.navegacion--ul');
 
+
+
 toogleDos.addEventListener('click', () => {
     sideBar.style.transform = "translateX(0)";
     toogleDos.style.display = "none";
@@ -26,7 +28,41 @@ nav.addEventListener('click', () => {
     sideBar.style.transform = "translateX(-100%)";
     toogleUno.style.display = "none";
     upto.style.display = "block";
-    toogleDos.style.display = "block";
     cuerpo.style.overflow = " ";
+    toogleDos.style.display = "block";
     cuerpo.style.overflow = "visible";
 });
+
+/* Eliminando parte del DOM */
+
+function moverNavSegunAncho() {
+
+    let sizeWindow = window.innerWidth;
+    const navegador = document.querySelector("nav");
+    const header = document.querySelector("header");
+
+    if (sizeWindow > 768) {
+        if (!header.contains(navegador)) {
+            var fragment = document.createDocumentFragment();
+            fragment.appendChild(navegador);
+            header.appendChild(fragment);
+            nav.removeAttribute('id');
+            
+        }
+    } else {
+        // Si quieres que el <nav> vuelva a su lugar original, debes tener una referencia al contenedor original
+        const contenedorOriginal = document.querySelector("#contenedor-original");
+
+        if (contenedorOriginal && !contenedorOriginal.contains(navegador)) {
+            var fragment = document.createDocumentFragment();
+            fragment.appendChild(navegador);
+            contenedorOriginal.appendChild(fragment);
+        }
+    }
+}
+
+// Ejecuta la función inicialmente
+moverNavSegunAncho();
+
+// Ejecuta la función cada vez que la ventana cambie de tamaño
+window.addEventListener('resize', moverNavSegunAncho);
